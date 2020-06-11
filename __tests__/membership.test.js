9db9025a71e7f833363319a1b179b5a37a039469
Max Lamb <maxlambcodes@gmail.com>
2020-06-10 18:03:07 -0700

    list all orgs

--- __tests__/membership.test.js
+++ __tests__/membership.test.js
@@ -92,15 +92,73 @@ describe('membership routes', () => {
       .get(`/api/v1/memberships?organization=${organization._id}`)
       .then(res => {
         expect(res.body).toEqual([{
+          __v: 0,
           _id: expect.anything(),
-          organization: organization.id,
-          user: user.id,
-          __v: 0         
-        }, {
+          organization: {
+            _id: expect.anything(),
+            image: 'placekitten.com',
+            title: 'Poll People',
+          },
+          user: {
+            _id: expect.anything(),
+            email: 'sosuperrad@sickness.gov',
+            name: 'Bing Bing',
+          },
+        },
+        {
+          __v: 0,
           _id: expect.anything(),
-          organization: organization.id,
-          user: user2.id,
-          __v: 0
+          organization: {
+            _id: expect.anything(),
+            image: 'placekitten.com',
+            title: 'Poll People',
+          },
+          user: {
+            _id: expect.anything(),
+            email: 'sickdaddy@ill.com',
+            name: 'Ding Ding',
+          },
+             
+        }]);
+      });
+  });
+  
+  it.only('lists all organizations a user is part of', async() => {
+
+    const organization = await Organization.create({
+      title: 'Poll People',
+      description: 'People Who Like Polls',
+      image: 'placekitten.com'
+    });
+
+    const organization2 = await Organization.create({
+      title: 'Super Crew',
+      description: 'People Who Are Super',
+      image: 'placekitten.com'
+    });
+
+    const user = await User.create({
+      name: 'Bing Bing',
+      phone: '4066666666',
+      email: 'sosuperrad@sickness.gov',
+      communicationMedium: 'email'
+    });
+
+    await Membership.create({
+      organization: organization._id,
+      user: user._id
+    });
+
+    await Membership.create({
+      organization: organization2._id,
+      user: user._id
+    });
+      
+    return request(app)
+      .get(`/api/v1/memberships?user=${user._id}`)
+      .then(res => {
+        expect(res.body).toEqual([{
+         
         }]);
       });
   });  
